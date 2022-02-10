281cb115dcd26830f77e4e6c77ef12cf142369a8
J.A. Aartsen <jamal.aartsen@hva.nl>
2022-02-10 13:56:44 +0100

    Change footer copyright text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
                 <FooterImg link="https://www.linkedin.com/in/jamal-aartsen-35a47615b/" image={LinkedinImage} imageTextAlt="linkedin-img" />
                 <FooterImg link="https://www.instagram.com/ijamall_/" image={InstagramImage} imageTextAlt="instagram-img" />
                 <FooterImg link="https://github.com/JamalAartsen?tab=repositories" image={GithubImage} imageTextAlt="github-img" />
-                <p className="footer-text">&copy; {new Date().getFullYear()} Jamal Aartsen</p>
+                <p className="footer-text">Copyright © 2021 Design & Development by Jamal Aartsen</p>
                 <p className="icons8-text">Icons provided by <a className="icons8-link" href="https://icons8.com/">icons8</a></p>
             </footer>
         </div>
